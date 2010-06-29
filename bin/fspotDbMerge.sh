92832c62e316ddce5fe5aedbe05548061d4b345d
Vitaly Kuznetsov <vitty@altlinux.ru>
2010-06-29 20:31:42 +0400

    add shell runner

--- bin/fspotDbMerge.sh
+++ bin/fspotDbMerge.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
 
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+CLASSPATH=$CLASSPATH:$bin/../lib/sqlite4java.jar:$bin/../lib/java-getopt-1.0.13.jar:$bin/../build/
+java -classpath $CLASSPATH fspotDbMerge $@
